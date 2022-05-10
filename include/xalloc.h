2bc6f199d5a6817e88303da0fa174da21448dc86
kcmw3e <kcmw3e@gmail.com>
2022-05-09 22:46:45 -0400

    add xmemset and xmemcpy and xbyte_t

--- include/xalloc.h
+++ include/xalloc.h
@@ -16,6 +16,7 @@
 
 #include <stdlib.h>
 
+typedef char xbyte_t;
 
 // wrappers
 // ---------------------------------------------------------------------------------------------- //
@@ -23,6 +24,10 @@ void* xmalloc(size_t size);
 void* xcalloc(size_t size, size_t num);
 void* xrealloc(void* p, size_t size);
 
+// memory utility
+// ---------------------------------------------------------------------------------------------- //
+void xmemset(xbyte_t mem[], xbyte_t b, size_t len);
+void xmemcpy(xbyte_t src[], xbyte_t cpy[], size_t len);
 
 
 // function descriptions
@@ -39,5 +44,29 @@ void* xcalloc(size_t size, size_t num);
 // description
 // |> same as [realloc] but throws allocation errors
 void* xrealloc(void* p, size_t size);
+
+// description
+//  |> sets the bytes in [mem] to [b]
+// parameters
+//  |> [mem]: an array of bytes to be changed
+//  |> [b]: the value to set each byte in [mem] to
+//  |> [len]: minimum length of [mem]
+// notes
+//  |> if [mem] is passed as [NULL] [xerr()] will be called and nothing else will happen
+//  |> all bytes in [mem] will be changed up to and including [len - 1]
+void xmemset(xbyte_t mem[], xbyte_t b, size_t len);
+
+// description
+//  |> copies the bytes in [src] to [cpy]
+// parameters
+//  |> [src]: an array of bytes containing the memory to be copied
+//  |> [cpy]: an array of bytes to serve as the destination of the copied memory
+//  |> [len]: minimum length of both [src] and [cpy]
+// notes
+//  |> if [src] or [cpy] is passed as [NULL] [xerr()] will be called and nothing else will happen
+//  |> all bytes in [cpy] will be changed up to and including [len - 1] using the corresponding
+//  |   bytes in [src]
+void xmemcpy(xbyte_t src[], xbyte_t cpy[], size_t len);
+
 
 #endif // CUTI_XALLOC_H
--- src/xalloc.c
+++ src/xalloc.c
@@ -18,3 +18,21 @@ void* xrealloc(void* p, size_t size) {
     if (p == NULL) xerr(XERR_ALLOCATION_FAIURE);
     return p;
 }
+
+void xmemset(xbyte_t mem[], xbyte_t b, size_t len) {
+    if (mem == NULL) {
+        xerr(XERR_NULL_CHECK_FAILED);
+        return;
+    }
+
+    for (size_t i = 0; i < len; i++) mem[i] = b;
+}
+
+void xmemcpy(xbyte_t src[], xbyte_t cpy[], size_t len) {
+    if (src == NULL || cpy == NULL) {
+        xerr(XERR_NULL_CHECK_FAILED);
+        return;
+    }
+
+    for (size_t i = 0; i < len; i++) cpy[i] = src[i];
+}
