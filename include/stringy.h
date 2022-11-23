fd923f9027f94a03a13de67a1b2354b9dd60e070
kc <kcmw3e@gmail.com>
2022-11-23 00:52:10 -0500

    changed string representation
    strings are now struct with start and end pointers, which accommodates
    the possibility of unknown-length strings to start from as well as non
    nul-terminated strings (if that's even useful)

--- include/stringy.h
+++ include/stringy.h
@@ -18,115 +18,29 @@
 #include <stdbool.h>
 
 
-typedef char* str_t;
+typedef struct str str_t;
 
+struct str {
+    const char* s;
+    const char* e;
+};
 
-// creation and freeing
+// creation
 // ---------------------------------------------------------------------------------------------- //
-str_t str_new(size_t len);
-str_t str_snew(const char* C, size_t len);
-void str_free(str_t str);
-
+str_t str_new(const char* s, size_t len);
 
 // manipulation
 // ---------------------------------------------------------------------------------------------- //
-void str_cpy(str_t str, str_t cpy, size_t n);
-
+str_t str_cpy(str_t str, char* dest, size_t len);
 
 // utility
 // ---------------------------------------------------------------------------------------------- //
-size_t str_len(str_t str, size_t n);
-bool str_eq(str_t str1, str_t str2, size_t n);
-bool str_in_strs(str_t str, str_t* strs_a, size_t strs_len, size_t n);
-
+size_t str_len(str_t str);
+bool str_eq(str_t str1, str_t str2);
+bool str_cont(str_t cont, str_t test);
+bool str_in_strs(str_t str, str_t* strs, size_t len);
 
 // function descriptions
 // ---------------------------------------------------------------------------------------------- //
-
-// description
-//  |> allocates a new [str_t] object
-// parameters
-//  |> [len]: length of string to be allocated, not including the terminating nul character
-// returns
-//  |> a [str_t] string initialized to 0 that can be used in any cuti library function, or [NULL] if
-//  |   error
-// notes
-//  |> if [NULL] is returned an allocation error occurred
-str_t str_new(size_t len);
-
-// description
-//  |> allocates a new [str_t] object initialized to the string in [char_a]
-// parameters
-//  |> [char_a]: a character array containing the string to initialize the new [str_t] with, must
-//  |   not be NULL
-//  |> [len]: length of string to be allocated and the minimum length of [char_a], not including
-//  |   any terminating nul character
-// return
-//  |> a [str_t] string that can be used in any cuti library functions, or [NULL] if error
-// notes
-//  |> if [NULL] is returned an allocation error occurred
-//  |> if the nul character was not reached after [len] characters in [char_a], the returned string
-//  |   will have all characters in [char_a] leading up to [char_a[len]] followed by a nul character
-//  |   such that the new string is nul-terminated
-str_t str_snew(const char* char_a, size_t len);
-
-// description
-//  |> frees a valid [str_t]
-// parameters
-//  |> [str]: a valid [str_t] or [NULL]
-void str_free(str_t str);
-
-// description
-//  |> get the length of string [str] within the range [n]
-// parameters
-//  |> [str]: a valid [str_t] to retrieve the length of
-//  |> [n]: the minimum length of [str], including any terminating nul character
-// return
-//  |> the number of characters in [str] before the first nul character, or [n] if a nul character
-//  |   was not reached
-size_t str_len(str_t str, size_t n);
-
-// description
-//  |> copy the string [str] to [cpy] within the range [n]
-// parameters
-//  |> [str]: a valid [str_t] to copy
-//  |> [cpy]: a valid [str_t] that serves as the destination to copy to
-//  |> [n]: the minimum length of both [str] and [cpy], including any terminating nul character
-// notes
-//  |> if a nul character is not reached by [n] characters, cpy will still contain the nul
-//  |   character in the [n - 1] position
-void str_cpy(str_t str, str_t cpy, size_t n);
-
-// description
-//  |> compares the first [n] characters in two strings to check for equality
-// parameters
-//  |> [str1]: the first string to compare
-//  |> [str2]: the second string to compare
-//  |> [n]: the minimum number of characters in [str1] and [str2], including any terminating nul
-//  |   character
-// return
-//  |> [true] if all characters in [str1] equal the characters in the same positions of [str2], or
-//  |   [false] if any character differs
-// notes
-//  |> the comparison stops after the first nul character is reached so that any difference in
-//  |   characters following is not considered
-//  |> only the first [n] characters are checked, so should [str1] or [str2] contain more characters
-//  |   they will not be checked
-bool str_eq(str_t str1, str_t str2, size_t n);
-
-// description
-//  |> checks whether a string is contained in an array of strings
-// parameters
-//  |> [str]: the string to look for
-//  |> [strs_a]: an array of strings to look in for [str]
-//  |> [strs_len]: the length of the array [strs_a]
-//  |> [n]: the minimum length of any string supplied whether [str] or one of [strs_a], including
-//  |   any nul-terminating character
-// return
-//  |> [true] if [str] could be matched to any string in [strs] or [false] otherwise
-// notes
-//  |> calls [str_eq()] on each string in [strs_a] until [str] is found or all the strings in
-//  |   [strs_a] have been checked
-bool str_in_strs(str_t str, str_t* strs_a, size_t strs_len, size_t n);
 
 #endif // CUTI_STRINGY_H
--- src/stringy.c
+++ src/stringy.c
@@ -4,62 +4,51 @@
 
 #define nul '\0'
 
-str_t str_new(size_t len) {
-    str_t str = xcalloc(len + 1, sizeof(*str));
+str_t str_new(const char* s, size_t len) {
+    str_t str = {
+        .s = s,
+        .e = s + len,
+    };
     return str;
 }
 
-str_t str_snew(const char* char_a, size_t len) {
-    if (char_a == NULL) return NULL;
+size_t str_len(str_t str) {
+    return str.e - str.s;
+}
 
-    str_t str = xmalloc(sizeof(*str)*(len + 1));
-    if (str == NULL) return NULL;
-    
-    for (size_t i = 0; i < len + 1; i++) {
-        char c = char_a[i];
-        str[i] = c;
-        if (c == nul) break;
+str_t str_cpy(str_t str, char* dest, size_t len) {
+    str_t cpy = str_new(dest, len);
+
+    while (str.s < str.e && dest < cpy.e) {
+        *(dest) = *(str.s);
+        str.s++;
+        dest++;
     }
 
-    str[len] = nul;
-
-    return str;
+    return cpy;
 }
 
-void str_free(str_t str) {
-    free(str);
-}
-
-size_t str_len(str_t str, size_t n) {
-    for (size_t len = 0; len < n; len++) {
-        char c = str[len];
-        if (c == nul) return len;
-    }
-    return n;
-}
-
-void str_cpy(str_t str, str_t cpy, size_t n) {
-    for (size_t i = 0; i < n; i++) {
-        char c = str[i];
-        cpy[i] = c;
-        if (c == nul) return;
-    }
-    cpy[n - 1] = nul;
-}
-
-bool str_eq(str_t str1, str_t str2, size_t n) {
-    for (size_t i = 0; i < n; i++) {
-        char c1 = str1[i];
-        char c2 = str2[i];
-        if (c1 != c2) return false;
-        if (c1 == nul) return true;
+bool str_eq(str_t str0, str_t str1) {
+    while (str0.s < str0.e && str1.s < str1.e) {
+        if (*(str0.s) != *(str1.s)) return false;
+        str0.s++;
+        str1.s++;
     }
     return true;
 }
 
-bool str_in_strs(str_t str, str_t* strs_a, size_t strs_len, size_t n) {
-    for (size_t i = 0; i < strs_len; i++) {
-        if (str_eq(str, strs_a[i], n)) return true;
+bool str_cont(str_t cont, str_t test) {
+    while (cont.s < cont.e) {
+        if (str_len(cont) < str_len(test)) return false; // in this case, there is no more room for test to exist in cont
+        if (str_eq(cont, test)) return true;
+        cont.s++;
+    }
+    return false;
+}
+
+bool str_in_strs(str_t str, str_t* strs, size_t len) {
+    for (size_t i = 0; i < len; i++) {
+        if (str_eq(str, strs[i])) return true;
     }
     return false;
 }
